a50ba29ead07c1e3959d18b0cbb8610be284a14e
Caroline <carolinechavezt@outlook.com>
2021-03-09 17:54:52 -0500

    commit tree 6c9b787f68d718d2644824c4e5213273be7beda9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,30 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".") 
-      true
-    else 
-      false
-    end
+    self.end_with?(".")
   end
 
   def question?
-    if self.end_with?("?")
-      true
-    else
-      false
-    end
+    self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
-      true
-    else
-      false
-    end
+    self.end_with?("!")
   end
 
   def count_sentences
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    self.split(/[.!?]+/).count
   end
 end
